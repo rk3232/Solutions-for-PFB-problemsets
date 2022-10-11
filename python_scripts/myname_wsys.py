8ab6b0017ecc6442153fcae07b6860145f7ac1c1
student <rkellemeyer@stowers.org>
2022-10-11 10:53:21 -0400

    adding myname with sys script

---
+++ python_scripts/myname_wsys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+myname = sys.argv[1]
+mycolor = sys.argv[2]
+myactivity = sys.argv[3]
+myanimal = sys.argv[4]
+
+print('My name is', myname, 
+	'\nMy favorite color is', mycolor, 
+	'\nMy favorite activity is', myactivity,
+	'\nMy favorite animal is', myanimal)
