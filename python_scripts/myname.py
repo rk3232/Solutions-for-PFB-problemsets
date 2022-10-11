2d77e220dbca27b6d4a6474511d45a9e6235cb42
student <rkellemeyer@stowers.org>
2022-10-11 10:31:18 -0400

    added name to print script

---
+++ python_scripts/myname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+#above calls on the specific python to use this for
+
+user_name = 'Riley'
+fave_color = 'orange'
+
+print('My name: ',user_name,
+'\nMy favorite color: ',fave_color)
+
+
