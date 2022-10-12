d4a92cdaac31bfc75bd22c683ee3baed1c456368
student <rkellemeyer@stowers.org>
2022-10-12 13:54:00 -0400

    Add fixes to EcoRI_sites.py

--- python_scripts/EcoRI_sites.py
+++ python_scripts/EcoRI_sites.py
@@ -8,16 +8,16 @@ site_input = sys.argv[1]
 og_seq = site_input.upper()
 
 if 'GAATTC' in og_seq:
-	print('EcoRI site present')
+	print('For your input, EcoRI site is present')
 	print(f'EcoRI site starts at position: {og_seq.index("GAATTC")} and ends at position: {og_seq.index("GAATTC") + 6}')
 else:
-	print('EcoRI site not present')
+	print('EcoRI site not present in your sample')
 
 #test
 seq1 = 'CCCTGTGGAATTCGTCAAG'
 
 if 'GAATTC' in seq1:
-	print('EcoRI site present')
+	print(f'For {seq1}, EcoRI site is present')
 	print(f'EcoRI site starts at position: {seq1.index("GAATTC")} and ends at position: {seq1.index("GAATTC") + 6}') 
 else:
 	print('EcoRI site not present')
