671b42654f8871d562800aee3cc9a5f2295fa347
student <rkellemeyer@stowers.org>
2022-10-13 10:51:34 -0400

    update file structure

--- python_scripts/python2unit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-num_list = [15, 21, 225]
-#string_list = [ '15', '21', '225' ]
-mix_list = [15, '21', 225]
-
-if num_list in mix_list: 
-	print("true")
-else:
-	print("not true")	
