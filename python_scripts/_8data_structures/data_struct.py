af62f5569958b400ac6ffe055c1925ba1a08ba21
student <rkellemeyer@stowers.org>
2022-10-16 22:03:35 -0400

    add dict within dict fasta

--- python_scripts/_8data_structures/data_struct.py
+++ python_scripts/_8data_structures/data_struct.py
@@ -22,37 +22,48 @@ print('User provided file: ',fasta_input)
 #fasta_dictionary with header:sequence
 #gene_ID dictionary with header:description
 fasta_dict = {}
-header = ''
+gene = {}
+#fasta_dict= {header:{desc:words, seq= sequence, {A:A_ ntcount,... }}}
 description = ''
-gene_info = [] 
 sequence = ''
+desc = 'desc'
+seq = 'seq'
 with open(fasta_input, 'r') as fasta_read:
 	for line in fasta_read:
 		line = line.rstrip()
 		if re.search(r'^>', line):
 			for obj in re.finditer(r'^>(\S*)\s(.*)', line):
-				header = obj.group(1)
-				description=obj.group(2)
-				gene_info = [description, sequence]
-				fasta_dict[header]=gene_info
+				gene = obj.group(1)
+				description = obj.group(2)
+				fasta_dict[gene]={desc:description, seq: sequence}
 		else:
 			sequence = line
-			fasta_dict[header][1]+=sequence			
+			fasta_dict[gene][seq]+=sequence			
 print(fasta_dict)
 
 #for each seq, count and add dictionary to list gene_info.append{nt:nt_count}
 
-#seq nt nt_count:
-#define nt and nt_count
-#nt_dict[nt] = nt_count
-nt= ''
-nt_count = []
+#check length of sequences
+for gene_key in fasta_dict:
+#	print(fasta_dict[key])
+	inner_seq = fasta_dict[gene_key]['seq']
+	print(len(inner_seq))
+	#print(len())
 
-for seq in fasta_dict[header][1]:
-	print(seq)	
-	
+#for each gene in dictionary	
+for gene_key in fasta_dict:
+	inner_seq = fasta_dict[gene_key]['seq']
+	nt_key= {}
+	for nt in inner_seq:
+		if nt in nt_key:
+			nt_key[nt]+=1
+		else:
+			nt_key[nt]=1
+#	gene_key[nt_key]+=nt #this doesn't workt
+	print(nt_key)
+print(fasta_dict)
 
-#ultra_fasta = {}
+
 
 
 
